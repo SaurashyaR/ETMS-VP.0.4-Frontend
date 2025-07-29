bc482eda422ae48ea2da7d7895dc497d28d1cfaf
saurashya <saurashya1@gmail.com>
2025-07-29 14:07:32 +0545

    folder setup

---
+++ src/app/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Dashboard from '@/components/features/dashboard/Dashboard'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <Dashboard/>
+    </div>
+  )
+}
+
+export default page
