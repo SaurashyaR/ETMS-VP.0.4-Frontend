756f4dde359ba76d092d8cf33a65bf2ad1ce2876
ashim <magarashim69086@gmail.com>
2025-07-31 14:52:05 +0545

    ui:basic ui implementaion

--- src/components/features/shared/SideBar.tsx
+++ src/components/features/shared/SideBar.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div className='w-[265px] border border-white
-      bg-blue-600
-      shadow-lg'>sidebar</div>
+    <div>SideBar</div>
   )
 }
 
